105b3388daa40f2a29486bd0b53958d98b75096f
imarneanu <iulia.marneanu@appsrise.com>
2016-07-20 17:58:00 +0300

    no message

---
+++ app/src/main/java/com/imarneanu/android/newsapp/TopicsActivity.java
@@ -0,0 +1,15 @@
+package com.imarneanu.android.newsapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by imarneanu on 7/19/16.
+ */
+public class TopicsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topics);
+    }
+}
